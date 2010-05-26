jaystevens/scst-trunk

commit 31edf0111cd0fabdb0028896ab4f29ba50ad0377

Merge of the pr branch with additional fixes for problems found during extra testing
git-svn-id: svn://svn.code.sf.net/p/scst/svn/trunk@1714 d57e44dd-8a1f-0410-8b47-8ef2f437770f

srpt/src/ib_srpt.c
@@ -2137,7 +2137,7 @@ static int srpt_cm_req_recv(struct ib_cm_id *cm_id,
 
 	BUG_ON(!sdev->scst_tgt);
 	ch->scst_sess = scst_register_session(sdev->scst_tgt, 0, ch->sess_name,
-					      NULL, NULL);
+					      ch, NULL, NULL);
 	if (!ch->scst_sess) {
 		rej->reason = cpu_to_be32(SRP_LOGIN_REJ_INSUFFICIENT_RESOURCES);
 		TRACE_DBG("%s", "Failed to create scst sess");
@@ -2146,8 +2146,6 @@ static int srpt_cm_req_recv(struct ib_cm_id *cm_id,
 
 	TRACE_DBG("Establish connection sess=%p name=%s cm_id=%p",
 		  ch->scst_sess, ch->sess_name, ch->cm_id);
-
-	scst_sess_set_tgt_priv(ch->scst_sess, ch);
 
 	/* create srp_login_response */
 	rsp->opcode = SRP_LOGIN_RSP;
@@ -2775,11 +2773,7 @@ static int srpt_xmit_response(struct scst_cmd *scmnd)
 	int resp_len;
 	enum srpt_command_state prev_state;
 
-	if (unlikely(scst_cmd_atomic(scmnd))) {
-		TRACE_DBG("%s", "Switching to thread context.");
-		ret = SCST_TGT_RES_NEED_THREAD_CTX;
-		goto out;
-	}
+	EXTRACHECKS_BUG_ON(scst_cmd_atomic(scst_cmd));
 
 	ioctx = scst_cmd_get_tgt_priv(scmnd);
 	BUG_ON(!ioctx);
@@ -2971,8 +2965,8 @@ static int srpt_detect(struct scst_tgt_template *tp)
 /**
  * srpt_release() - SCST release callback function.
  *
- * Callback function called by the SCST core from scst_unregister() to free up
- * the resources associated with device scst_tgt.
+ * Callback function called by the SCST core from scst_unregister_target() to
+ * free up the resources associated with device scst_tgt.
  */
 static int srpt_release(struct scst_tgt *scst_tgt)
 {
@@ -3157,7 +3151,7 @@ static void srpt_add_one(struct ib_device *device)
 	if (!sdev)
 		return;
 
-	sdev->scst_tgt = scst_register(&srpt_template, NULL);
+	sdev->scst_tgt = scst_register_target(&srpt_template, NULL);
 	if (!sdev->scst_tgt) {
 		PRINT_ERROR("SCST registration failed for %s.",
 			    sdev->device->name);
@@ -3306,7 +3300,7 @@ err_dev:
 	device_unregister(&sdev->dev);
 #endif
 unregister_tgt:
-	scst_unregister(sdev->scst_tgt);
+	scst_unregister_target(sdev->scst_tgt);
 free_dev:
 	kfree(sdev);
 
@@ -3366,7 +3360,7 @@ static void srpt_remove_one(struct ib_device *device)
 	 * such that no new SRP_LOGIN_REQ information units can arrive while
 	 * destroying the SCST target.
 	 */
-	scst_unregister(sdev->scst_tgt);
+	scst_unregister_target(sdev->scst_tgt);
 	sdev->scst_tgt = NULL;
 
 	srpt_free_ioctx_ring(sdev, sdev->ioctx_ring,

Makefile
@@ -82,6 +82,18 @@ help:
 	@echo "		srpt_install      : srp target: install"
 	@echo "		srpt_uninstall    : srp target: uninstall"
 	@echo ""
+	@echo "		mvsas             : make MVSAS target"
+	@echo "		mvsas_clean       : mvsas target: clean "
+	@echo "		mvsas_extraclean  : mvsas target: clean + clean dependencies"
+	@echo "		mvsas_install     : mvsas target: install"
+	@echo "		mvsas_uninstall   : mvsas target: uninstall"
+	@echo ""
+	@echo "		fcst              : make FCoE target"
+	@echo "		fcst_clean        : FCoE target: clean "
+	@echo "		fcst_extraclean   : FCoE target: clean + clean dependencies"
+	@echo "		fcst_install      : FCoE target: install"
+	@echo "		fcst_uninstall    : FCoE target: uninstall"
+	@echo ""
 	@echo "		scst_local	  : make scst_local target"
 	@echo "		scst_local_install : scst_local target: install"
 	@echo "		scst_local_uninstall : scst_local target: uninstall"

fcst/ft_sess.c
@@ -56,7 +56,7 @@ static struct ft_tport *ft_tport_create(struct fc_lport *lport)
 	if (!tport)
 		return NULL;
 
-	tport->tgt = scst_register(&ft_scst_template, name);
+	tport->tgt = scst_register_target(&ft_scst_template, name);
 	if (!tport->tgt) {
 		kfree(tport);
 		return NULL;
@@ -94,7 +94,7 @@ static void ft_tport_delete(struct ft_tport *tport)
 	tgt = tport->tgt;
 	BUG_ON(!tgt);
 	FT_SESS_DBG("delete %s\n", scst_get_tgt_name(tgt));
-	scst_unregister(tgt);
+	scst_unregister_target(tgt);
 	lport = tport->lport;
 	BUG_ON(tport != lport->prov[FC_TYPE_FCP]);
 	rcu_assign_pointer(lport->prov[FC_TYPE_FCP], NULL);
@@ -214,7 +214,8 @@ static int ft_sess_create(struct ft_tport *tport, struct fc_rport_priv *rdata,
 
 	ft_format_wwn(name, sizeof(name), rdata->ids.port_name);
 	FT_SESS_DBG("register %s\n", name);
-	scst_sess = scst_register_session(tport->tgt, 0, name, NULL, NULL);
+	scst_sess = scst_register_session(tport->tgt, 0, name, sess, NULL,
+					  NULL);
 	if (!scst_sess) {
 		kfree(sess);
 		return FC_SPP_RESP_RES;		/* out of resources */
@@ -236,7 +237,6 @@ static int ft_sess_create(struct ft_tport *tport, struct fc_rport_priv *rdata,
 	sess->params = fcp_parm;
 
 	rdata->prli_count++;
-	scst_sess_set_tgt_priv(scst_sess, sess);
 	return 0;
 }
 
@@ -474,7 +474,7 @@ void ft_recv(struct fc_lport *lport, struct fc_seq *sp, struct fc_frame *fp)
 
 /*
  * Release all sessions for a target.
- * Called through scst_unregister() as well as directly.
+ * Called through scst_unregister_target() as well as directly.
  * Caller holds ft_lport_lock.
  */
 int ft_tgt_release(struct scst_tgt *tgt)

iscsi-scst/README
@@ -805,6 +805,13 @@ in/out in the kernel's module Makefile:
 
  - CONFIG_SCST_ISCSI_DEBUG_DIGEST_FAILURES - simulates digest failures in
    random places.
+ 
+ - CONFIG_SCST_ISCSI_SKIP_ISID - makes iSCSI-SCST to always use iSCSI
+   initiator port TransportID format 0, i.e. always identify initiators
+   only by names and ignore ISID part in TransportIDs. This is useful
+   for Persistent Reservations with initiators which don't use ISID in a
+   standards confirming way, for instance by always incrementing it on
+   each reconnect, as well as to debug Persistent Reservations.
 
 
 Creating version of put_page_callback patch for your kernel

iscsi-scst/README_in-tree
@@ -701,6 +701,13 @@ in/out in the kernel's module Makefile:
  - CONFIG_SCST_ISCSI_DEBUG_DIGEST_FAILURES - simulates digest failures in
    random places.
 
+ - CONFIG_SCST_ISCSI_SKIP_ISID - makes iSCSI-SCST to always use iSCSI
+   initiator port TransportID format 0, i.e. always identify initiators
+   only by names and ignore ISID part in TransportIDs. This is useful
+   for Persistent Reservations with initiators which don't use ISID in a
+   standards confirming way, for instance by always incrementing it on
+   each reconnect, as well as to debug Persistent Reservations.
+
 
 Credits
 -------

iscsi-scst/kernel/Kconfig
@@ -23,3 +23,17 @@ config SCST_ISCSI_DEBUG_DIGEST_FAILURES
 	  iSCSI initiator that is talking to SCST.
 
 	  If unsure, say "N".
+
+config CONFIG_SCST_ISCSI_SKIP_ISID
+	bool "Do not use ISID in initiator port transport id"
+	depends on SCST_ISCSI
+	help
+	  Makes iSCSI-SCST to always use iSCSI initiator port
+	  TransportID format 0, i.e. always identify initiators only by
+	  names and ignore ISID part in TransportIDs. This is useful for
+	  Persistent Reservations with initiators which don't use ISID
+	  in a standards confirming way, for instance by always
+	  incrementing it on each reconnect, as well as to debug
+	  Persistent Reservations.
+
+	  If unsure, say "N".

iscsi-scst/kernel/Makefile
@@ -29,6 +29,8 @@ EXTRA_CFLAGS += -DCONFIG_SCST_DEBUG -g -fno-inline -fno-inline-functions
 
 #EXTRA_CFLAGS += -DCONFIG_SCST_ISCSI_DEBUG_DIGEST_FAILURES
 
+#EXTRA_CFLAGS += -DCONFIG_SCST_ISCSI_SKIP_ISID
+
 obj-m		+= iscsi-scst.o
 iscsi-scst-objs	:= iscsi.o nthread.o config.o digest.o \
 	conn.o session.o target.o event.o param.o

iscsi-scst/kernel/config.c
@@ -260,6 +260,10 @@ static ssize_t iscsi_version_show(struct kobject *kobj,
 	strcat(buf, "DEBUG_DIGEST_FAILURES\n");
 #endif
 
+#ifdef CONFIG_SCST_ISCSI_SKIP_ISID
+	strcat(buf, "CONFIG_SCST_ISCSI_SKIP_ISID\n");
+#endif
+
 	TRACE_EXIT();
 	return strlen(buf);
 }

commit 55648cb6f0a1826bf658243fd23ccfa9da55442f

Fixed build breakage caused by r1714.
git-svn-id: svn://svn.code.sf.net/p/scst/svn/trunk@1715 d57e44dd-8a1f-0410-8b47-8ef2f437770f

srpt/src/ib_srpt.c
@@ -2773,7 +2773,7 @@ static int srpt_xmit_response(struct scst_cmd *scmnd)
 	int resp_len;
 	enum srpt_command_state prev_state;
 
-	EXTRACHECKS_BUG_ON(scst_cmd_atomic(scst_cmd));
+	EXTRACHECKS_BUG_ON(scst_cmd_atomic(scmnd));
 
 	ioctx = scst_cmd_get_tgt_priv(scmnd);
 	BUG_ON(!ioctx);